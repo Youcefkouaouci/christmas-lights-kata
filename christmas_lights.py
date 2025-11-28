6bf606dc50e8a8b9913194f2a0223015e15c7e21
Youcef Kouaouci <youcefkouaouci@gmail.com>
2025-11-28 22:42:16 +0100

    feat: ajouter les fonctions turn_on et turn_off

--- christmas_lights.py
+++ christmas_lights.py
@@ -22,3 +22,33 @@ class LightGrid:
         for row in self.grid:
             count += sum(row)
         return count
+    
+    def turn_on(self, x1, y1, x2, y2):
+        """
+        Allume les lumières dans une zone rectangulaire
+        
+        Args:
+            x1, y1: Coordonnées du coin supérieur gauche
+            x2, y2: Coordonnées du coin inférieur droit
+        """
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.grid[x][y] = True
+    
+    def turn_off(self, x1, y1, x2, y2):
+        """
+        Éteint les lumières dans une zone rectangulaire
+        
+        Args:
+            x1, y1: Coordonnées du coin supérieur gauche
+            x2, y2: Coordonnées du coin inférieur droit
+        """
+        for x in range(x1, x2 + 1):
+            for y in range(y1, y2 + 1):
+                self.grid[x][y] = False
+    
+    def count_lights_on(self):
+        count = 0
+        for row in self.grid:
+            count += sum(row)
+        return count
